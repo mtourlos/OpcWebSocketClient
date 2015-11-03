3264854d7c23382774d1a336ca63b59f77a67351
mtourlos <michaeltourlos@yahoo.gr>
2015-11-03 16:12:47 +0200

    registration to gcm

--- www/js/app.js
+++ www/js/app.js
@@ -18,7 +18,7 @@ app.run(function($ionicPlatform) {
   });
 });
  
-app.controller('NotificationController', function($scope, $cordovaLocalNotification, $ionicPlatform) {
+app.controller('NotificationController', function($scope, $cordovaLocalNotification, $cordovaPush, $cordovaToast, $ionicPlatform) {
      
     $ionicPlatform.ready(function () {
          
@@ -74,7 +74,78 @@ app.controller('NotificationController', function($scope, $cordovaLocalNotificat
           $cordovaLocalNotification.cancel(3).then(function (result) {
             console.log('Notification 3 Canceled');
           });
-        };      
-         
+        };
+		
+		//test method for setting the ui
+		$scope.setText = function (){
+			alert("SetText");
+			$scope.testText="this is a test";
+		};
+		
+		//push notification
+		
+		$scope.register = function () {
+			var config = null;
+
+			if (ionic.Platform.isAndroid()) {
+				config = {
+					"senderID": "AIzaSyB2YEaWFr0vTYjhjCzx0Ns39hE9Is5Z7n8" // REPLACE THIS WITH YOURS FROM GCM CONSOLE - also in the project URL like: https://console.developers.google.com/project/434205989073
+				};
+			}
+			else if (ionic.Platform.isIOS()) {
+				config = {
+					"badge": "true",
+					"sound": "true",
+					"alert": "true"
+				}
+			}
+
+			$cordovaPush.register(config).then(function (result) {
+				console.log("Register success " + result);
+
+				$cordovaToast.showShortCenter('Registered for push notifications');
+				//$scope.registerDisabled=true;
+				// ** NOTE: Android regid result comes back in the pushNotificationReceived, only iOS returned here
+				if (ionic.Platform.isIOS()) {
+					$scope.regId = result;
+					storeDeviceToken("ios");
+				}
+			}, function (err) {
+				console.log("Register error " + err)
+			});
+		};
+		
+		// Notification Received
+		$scope.$on('$cordovaPush:notificationReceived', function (event, notification) {
+			console.log(JSON.stringify([notification]));
+			if (ionic.Platform.isAndroid()) {
+				handleAndroid(notification);
+			}
+			else if (ionic.Platform.isIOS()) {
+				handleIOS(notification);
+				$scope.$apply(function () {
+					$scope.notifications.push(JSON.stringify(notification.alert));
+				})
+			}
+		});		
+		
+		// Android Notification Received Handler
+		function handleAndroid(notification) {
+			// ** NOTE: ** You could add code for when app is in foreground or not, or coming from coldstart here too
+			//             via the console fields as shown.
+			console.log("In foreground " + notification.foreground  + " Coldstart " + notification.coldstart);
+			if (notification.event == "registered") {
+				$scope.regId = notification.regid;
+				//storeDeviceToken("android");
+			}
+			else if (notification.event == "message") {
+				$cordovaDialogs.alert(notification.message, "Push Notification Received");
+				$cordovaDialogs.alert(notification.message, JSON.stringify(notification));
+			}
+			else if (notification.event == "error")
+				$cordovaDialogs.alert(notification.msg, "Push notification error event");
+			else $cordovaDialogs.alert(notification.event, "Push notification handler - Unprocessed Event");
+		};
+		
     });
 });
